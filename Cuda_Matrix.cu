e10694ed5ec3e5b0769b022bf81a8f23332a9926
Daniel Gibney <dpgibney@gmail.com>
2018-10-22 17:27:25 -0400

    starting work

---
+++ Cuda_Matrix.cu
@@ -0,0 +1,13 @@
+#include<cuda.h>
+#include<cublas.h>
+#include<cuda_runtime.h>
+#include"Cuda_Matrix.h"
+
+template <typename T>
+class Cuda_Matrix{
+    Cuda_Matrix<T>();
+
+
+};
+
+
---
+++ Cuda_Matrix.h
@@ -0,0 +1,11 @@
+#ifndef PLUGIN_CUDA_MATRIX_H
+#define PLUGIN_CUDA_MATRIX_H
+
+#include<cuda.h>
+#include<cublas.h>
+#include<cuda_runtime.h>
+
+template <typename T>
+Cuda_Matrix<T>::Cuda_Matrix<T>();
+
+#endif
